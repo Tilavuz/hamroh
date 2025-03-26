88bf79395009525dbf35beb6d64c570c744f3fe0
Tilav <123973896+Tilavuz@users.noreply.github.com>
2025-03-26 09:20:49 +0500

    Update settings.py

--- config/settings.py
+++ config/settings.py
@@ -92,19 +92,19 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
-import dj_database_url
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+# import dj_database_url
 
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
 DATABASES = {
-    'default': dj_database_url.parse('postgresql://postgres:zWFTpdfwAEocNrNOgNAGmHZWtVhiaZwC@junction.proxy.rlwy.net:22923/railway')
+    'default': dj_database_url.parse('postgresql://postgres:nOxXxdpgBLiEYhoawdNIEmytgaMQjWGl@hopper.proxy.rlwy.net:24241/railway')
 }
 
 # Password validation
